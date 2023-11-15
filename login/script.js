98c7f55874dfc53418eb0be1e678514f60471362
sghost31 <148738232+sghost31@users.noreply.github.com>
2023-11-15 14:40:16 +0800

    Update script.js

--- login/script.js
+++ login/script.js
@@ -32,3 +32,19 @@
     login.addEventListener("click", ( )=>{
         container.classList.remove("active");
     });
+
+//john modificatio
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "login.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("result").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send("username=" + username + "&password=" + password);
+}
