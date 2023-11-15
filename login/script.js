1a67c32840c715316c8a7f5dabcb0a768bb030e1
sghost31 <148738232+sghost31@users.noreply.github.com>
2023-11-15 15:43:02 +0800

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,4 +1,4 @@
- /*   const container = document.querySelector(".container"),
+   const container = document.querySelector(".container"),
       pwShowHide = document.querySelectorAll(".showHidePw"),
       pwFields = document.querySelectorAll(".password"),
       signUp = document.querySelector(".signup-link"),
@@ -31,44 +31,4 @@
     });
     login.addEventListener("click", ( )=>{
         container.classList.remove("active");
-    });*/
-
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 5000;
-
-app.use(bodyParser.json());
-
-const db = mysql.createConnection({
-    host: '140.112.121.85',
-    user: 'root',
-    password: 'rtlab666',
-    database: 'dashboard',
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    // Perform database query using username and password
-    const query = 'SELECT * FROM account WHERE s_name = ? AND s_password = ?';
-    db.query(query, [s_name, s_password], (err, results) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            if (results.length > 0) {
-                res.status(200).json({ message: 'Login successful' });
-            } else {
-                res.status(401).json({ error: 'Invalid credentials' });
-            }
-        }
     });
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-
