8dea84ce0bc8eb9bcb7dce99d63e7e4e6104dbbe
sghost31 <148738232+sghost31@users.noreply.github.com>
2023-11-15 15:00:22 +0800

    Update script.js

--- login/script.js
+++ login/script.js
@@ -40,7 +40,7 @@ function login() {
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "login.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             document.getElementById("result").innerHTML = xhr.responseText;
