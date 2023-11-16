8048c358d02b159130523a4999cd212aee7eea7d
saffanazyan <131111603+saffanazyan@users.noreply.github.com>
2023-11-16 16:48:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,32 +33,61 @@
         container.classList.remove("active");
     });
 
-//signUp.addEventListener("button", function (e) {
-    //e.preventDefault();
-
-    //let s_email = document.getElementById("s_email").value;
-    //window.sessionStorage.setItem("s_email", s_email);
-    //swal("Loading Now", "", "info", { timer: 2000, showConfirmButton: false });
-
-    //var formdata = new FormData(document.getElementById("form signup"));
-    //fetch("http://140.118.121.85:5000/user/signup", {
-        //method: "POST",
-        //body: formdata,
-    //})
-        //.then((response) => {
-            //return response.json();
-        //})
-        //.then((data) => {
-            //Signuprender(data);
-        //})
-        //.catch((error) => console.log("error", error));
-//});
-
-//function Signuprender(data) {
-    //let market = data.log;
-
-    //if (market == "0") {
-        //swal("Success", "Sign up successfully! Please verify your email.", "success", { timer: 2000, showConfirmButton: false });
-        //showVerifyConference();
+document.querySelector('#signUp input[type="button"]').addEventListener('click', async function(e) {
+    e.preventDefault();
+  
+    const name = document.getElementById('s_name').value;
+    const email = document.getElementById('s_email').value;
+    const password = document.getElementById('s_password').value;
+    const passwordConfirm = document.getElementById('s_password confirm').value;
+    const phone = document.getElementById('s_phone').value;
+  
+    if (!password || !passwordConfirm || password !== passwordConfirm) {
+      alert('Passwords do not match.');
+      return;
+    }
+  
+    const termCon = document.getElementById('termCon').checked;
+  
+    if (!termCon) {
+      alert('Please accept all terms and conditions.');
+      return;
+    }
+  
+    const user = {
+      name,
+      email,
+      password,
+      phone
+    };
+  
+    swal({
+      title: "Loading Now",
+      timer: 2000,
+      onOpen: () => {
+        swal.showLoading();
+      }
+    });
+  
+    try {
+      const response = await fetch('http://140.118.121.85:5000/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      });
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      Signuprender(data);
+      
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+});
 
 
