4fa151f2c77142ff76de5a4d8b41b4c592d9a1b2
saffanazyan <131111603+saffanazyan@users.noreply.github.com>
2023-11-16 19:54:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -150,3 +150,14 @@ function isValidEmail(email) {
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
+// Fetch data from the server
+  fetch('/fetchData')
+    .then(response => response.json())
+    .then(data => {
+      // Assuming the 'message' property exists in the fetched data
+      const alertMessage = data[0].message;
+
+      // Display an alert with the fetched message
+      alert(alertMessage);
+    })
+    .catch(error => console.error('Error fetching data: ', error));
