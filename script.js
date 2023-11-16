578aa03a45b902fa547642dc05eb5a770aad51bf
saffanazyan <131111603+saffanazyan@users.noreply.github.com>
2023-11-16 15:56:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,39 @@
     })
 
     // js code to appear signup and login form
-    signUp.addEventListener("click", ( )=>{
-        container.classList.add("active");
+    //signUp.addEventListener("click", ( )=>{
+        //container.classList.add("active");
     });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
+    //login.addEventListener("click", ( )=>{
+        //container.classList.remove("active");
     });
+
+signUp.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let s_email = document.getElementById("s_email").value;
+    window.sessionStorage.setItem("s_email", s_email);
+    swal("Loading Now", "", "info", { timer: 2000, showConfirmButton: false });
+
+    var formdata = new FormData(document.getElementById("myForm"));
+    fetch("http://140.118.121.85:5000/user/signup", {
+        method: "POST",
+        body: formdata,
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            Signuprender(data);
+        })
+        .catch((error) => console.log("error", error));
+});
+
+function Signuprender(data) {
+    let market = data.log;
+
+    if (market == "0") {
+        swal("Success", "Sign up successfully! Please verify your email.", "success", { timer: 2000, showConfirmButton: false });
+        showVerifyConference();
+
+
