b8bb21e5b810d5b37234b6fe24712ccd4c784a80
saffanazyan <131111603+saffanazyan@users.noreply.github.com>
2023-11-17 16:46:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,64 +33,6 @@
         container.classList.remove("active");
     });
 
-//signup call API
-/* document.getElementById('signUp').addEventListener('click', async function(e) {
-    e.preventDefault();
-  
-    const s_name = document.getElementById('s_name').value;
-    const s_email = document.getElementById('s_email').value;
-    const s_password = document.getElementById('s_password').value;
-    const s_passwordconfirm = document.getElementById('s_password confirm').value;
-    const s_phone = document.getElementById('s_phone').value;
-  
-   if (!password || !passwordConfirm || password !== passwordConfirm) {
-      alert('Passwords do not match.');
-      return;
-    }
-  
-    const termCon = document.getElementById('termCon').checked;
-  
-    if (!termCon) {
-      alert('Please accept all terms and conditions.');
-      return;
-    }
-  
-    const user = {
-      s_name,
-      s_email,
-      s_password,
-      s_phone
-    };
-  
-    swal({
-      title: "Loading Now",
-      timer: 2000,
-      onOpen: () => {
-        swal.showLoading();
-      }
-    });
-  
-    try {
-      const response = await fetch('http://140.118.121.85:5000/user/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-      });
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      
-      const data = await response.json();
-      Signuprender(data);
-      
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-});*/
-
 function submitForm() {
     var name = document.getElementById("s_name").value;
     var email = document.getElementById("s_email").value;
