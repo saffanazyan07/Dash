5ed8d928105ac59a46fb82429358657ba7d31221
sghost31 <148738232+sghost31@users.noreply.github.com>
2023-11-18 14:10:29 +0800

    Update scripthome.js

--- scripthome.js
+++ scripthome.js
@@ -1,1 +1,22 @@
 
+//##########UEINFO
+const apiUrl = 'http:// 140.118.121.85:5000/ue/info';
+
+// Fetch data from the API
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Handle the retrieved data
+        console.log(data);
+
+        // Call a function to display the data
+        displayData(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+// Function to display data in the HTML
+function displayData(data) {
+    // Access data properties and update HTML elements
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.innerHTML = `<p>${data.propertyName}</p>`;
+    // Add more HTML manipulation as needed based on the data structure
