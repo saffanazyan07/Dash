12b0c633e331f8e93fa08b40af983e84ac8573de
saffanazyan <131111603+saffanazyan@users.noreply.github.com>
2024-01-04 06:54:28 +0800

    Update index.js

--- USER/index.js
+++ USER/index.js
@@ -9,16 +9,16 @@ sideLinks.forEach(item => {
         li.classList.add('active');
     })
 });
-
+/*dropdown*/
 function hoverTabs(evt, cityName) {
-  var i, tabcontent, tablinks;
+  var i, tabcontent, sidemenulinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  sidemenulinks = document.getElementsByClassName("sidemenulinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    sidemenulinks[i].className = sidemenulinks[i].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
